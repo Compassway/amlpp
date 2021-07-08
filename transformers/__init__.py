a71f865b6f0f966d3c43227f38e3b62a72b75918
Asirg <vojt.tieg295i@gmail.com>
2021-07-08 14:08:15 +0300

    fix problem with deleted columns in iterativeImputer

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -4,4 +4,4 @@ from ._transformers import CategoricalEncoder
 from ._transformers import Word2Vectorization
 from ._transformers import ImputerValue
 from ._transformers import ImputerIterative
-from ._transformers import CheckXY
+# from ._transformers import CheckXY
