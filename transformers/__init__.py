bb413b05fc49959fa7ab6b02ca7128f3e08f3ef4
Asirg <vojt.tieg295i@gmail.com>
2021-07-11 10:53:50 +0300

    add set new structure for transformers in __init__ file for import

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -1,7 +1,9 @@
 import warnings
 warnings.filterwarnings("ignore")
-from ._transformers import CategoricalEncoder
-from ._transformers import Word2Vectorization
-from ._transformers import ImputerValue
-from ._transformers import ImputerIterative
-# from ._transformers import CheckXY
+
+from ._categorical import CategoricalEncoder
+
+from ._word_embeddings import Word2Vectorization
+
+from ._imputers import ImputerValue
+from ._imputers import ImputerIterative
